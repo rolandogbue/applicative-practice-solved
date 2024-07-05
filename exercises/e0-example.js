37cbec1c2484a900064a29d9325b9acdd486fc07
Roland <rolandajonathan@gmail.com>
2024-07-05 17:03:04 -0400

    solved

--- exercises/e0-example.js
+++ exercises/e0-example.js
@@ -11,8 +11,6 @@ export function getFirstPlanetName(data) {
   return planets[0].name;
 }
 
-
-
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-0"
 // If the test has all tests passed, switch to the next exercise file
--- exercises/e1.js
+++ exercises/e1.js
@@ -6,9 +6,9 @@ import { data } from "../data/data";
 
 export function getPlanetNames(data) {
   // Your code goes here...
+  const planets = data.planets;
+  return planets.map((planet) => planet.name);
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
